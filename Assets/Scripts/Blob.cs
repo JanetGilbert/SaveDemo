c0540c7b0a99c3702c9d2ed45fc35c009d230610
Janet Gilbert <janet@wadjeteyegames.com>
2020-04-07 16:42:17 -0400

    Added basic game (no saves)

---
+++ Assets/Scripts/Blob.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Blobs move back and forth between two points.
+ * If blobs are targets, they turn a different color, and shrink when clicked.
+ * If they shrink to nothing then they are destroyed.
+ * If blobs are clicked when they are not targets, they grow to a maximum size.*/
+public class Blob : MonoBehaviour
+{
+    // Settable in editor
+    [SerializeField]
+    [Header("Number of size steps until blob destroyed")]
+    private int maxSizeStep = 3;
+
+    [SerializeField]
+    [Header("Movement lerp speed")]
+    private float moveSpeed = 0.3f;
+
+
+    [SerializeField]
+    [Header("How fast the blobs scale up or down")]
+    private float scaleSpeed = 1.0f;
+
+    // Cached components
+    private BlobSpawner spawner;
+    private MeshRenderer meshRenderer;
+
+    // Scaling data
+    private int size;
+    private bool scaling = false;
+    private float scaleTo = 1.0f;
+    private float curScale = 1.0f;
+    
+    // Lerping data
+    private Vector3 start;
+    private Vector3 end;
+    private float lerpTime;
+
+    // Property with no backing variable to set material color.
+    public Color BlobColor
+    {
+        set // Setter only, there is no need for a getter.
+        {
+            meshRenderer.material.color = value;
+        }
+    }
+
+    /* Put things in Awake that don't depend on other objects.
+     This is important if you are instantiating the object containing a component, so that the object is initialized
+     and can be used straight away. */
+    void Awake()
+    {
+        // Cache useful components.
+        meshRenderer = GetComponent<MeshRenderer>(); 
+       
+
+        // Set movement pattern.
+        start = transform.position;
+        end = start + (Random.rotation * Vector3.forward) * Random.Range(1.0f, 5.0f);
+
+        size = maxSizeStep;
+    }
+
+    void Start()
+    {
+        spawner = GetComponentInParent<BlobSpawner>(); // Depends on other object, so set in Start()
+
+    }
+
+    void Update()
+    {
+        // Scale down after target blob is clicked, up after non-target blob clicked.
+        if (scaling)
+        {
+            if (curScale < scaleTo)
+            {
+                curScale += scaleSpeed * Time.deltaTime;
+
+                if (curScale >= scaleTo)
+                {
+                    scaling = false;
+                }
+            }
+            else if (curScale > scaleTo)
+            {
+                curScale -= scaleSpeed * Time.deltaTime;
+
+                if (curScale <= scaleTo)
+                {
+                    scaling = false;
+                }
+            }
+
+            transform.localScale = new Vector3(curScale, curScale, 1.0f);
+        }
+
+        // Lerp between two points.
+        Vector3 newPos;
+
+        if (lerpTime < 1.0f)
+        {
+            newPos = Vector3.Lerp(start, end, lerpTime);
+        }
+        else
+        {
+            newPos = Vector3.Lerp(end, start, lerpTime - 1.0f);
+        }
+
+        lerpTime += Time.deltaTime * moveSpeed;
+
+        if (lerpTime > 2.0f)
+        {
+            lerpTime = 0.0f;
+        }
+
+        transform.position = newPos;
+    }
+
+    
+    void OnMouseDown()
+    {
+        if (spawner.IsCurrent(this)) // If blob is target, decrease the size
+        {
+            size--;
+
+            if (size <= 0)
+            {
+                size = 0;
+
+                // Destroy blob when it shrinks to nothing.
+                spawner.Remove(this);
+                Destroy(this.gameObject); 
+            }
+        }
+        else // If blob is not target, increase the size
+        {
+            size++;
+
+            if (size > maxSizeStep)
+            {
+                size = maxSizeStep;
+            }
+        }
+
+        // Set scale target.
+        float scaled = 1.0f / maxSizeStep * size;
+        scaleTo = scaled;
+        scaling = true;
+    }
+
+
+}
---
+++ Assets/Scripts/BlobSpawner.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobSpawner : MonoBehaviour
+{
+    // Settable in editor
+    [SerializeField]
+    [Header("Set the blob prefab to spawn")]
+    private Blob prefabToSpawn = null;
+
+    [SerializeField]
+    [Header("Seconds between spawns")]
+    private float spawnTimeMax = 1.0f;
+
+    [SerializeField]
+    [Header("The color of the target blob")]
+    private Color activeColor = Color.red;
+
+    [SerializeField]
+    [Header("The color of the non-target blobs")]
+    private Color inActiveColor = Color.white;
+
+    [SerializeField]
+    [Header("Space at the edge of the screen where blobs are not spawned")]
+    public float borderWidth = 0.1f;
+
+    [SerializeField]
+    [Header("How often the target switches between blobs")]
+    private float switchTimeMax = 0.1f;
+
+    // Timers
+    private float spawnTimer;  // Countdown between spawns.
+    private float switchTimer; // Countdown between target blob switch.
+
+    // Blobs
+    private List<Blob> blobList = new List<Blob>(); // List of all blobs.
+    private Blob target; // Target Blob
+
+
+    void Start()
+    {
+        Spawn(); // Add one blob.
+        SetActive(blobList[0]); // Set initial target.
+    }
+
+    // Set the target, and set all other blobs to not be targets.
+    void SetActive(Blob newActive)
+    {
+        if (target != null)
+        {
+            target.BlobColor = inActiveColor;
+        }
+
+        target = newActive;
+        target.BlobColor = activeColor;
+    }
+
+    // Is this blob the current target?
+    public bool IsCurrent(Blob toCheck)
+    {
+        return toCheck == target;
+    }
+
+    
+    void Update()
+    {
+        // Spawn blobs every spawnTimeMax seconds.
+        spawnTimer -= Time.deltaTime;
+
+        while (spawnTimer < 0.0f)
+        {
+            spawnTimer += spawnTimeMax;
+
+            Spawn();
+        }
+
+
+        // Switch target every switchTimeMax seconds.
+        switchTimer -= Time.deltaTime;
+
+        if (switchTimer < 0.0f)
+        {
+            switchTimer = switchTimeMax;
+
+            SetActive(blobList[Random.Range(0, blobList.Count)]);
+        }
+    }
+
+    // Create blob and add it to the list.
+    void Spawn()
+    {
+        float halfBorder = borderWidth / 2.0f;
+
+        Vector2 pos = Camera.main.ViewportToWorldPoint(new Vector2(Random.Range(halfBorder, 1.0f - halfBorder),
+                                                                   Random.Range(halfBorder, 1.0f - halfBorder)));
+        Blob newBlob = Instantiate<Blob>(prefabToSpawn, new Vector3(pos.x, pos.y, 0.0f), Quaternion.identity);
+
+        newBlob.transform.parent = transform;
+
+        Blob target = newBlob.GetComponent<Blob>();
+        blobList.Add(target);
+    }
+
+    // Remove blob from list.
+    public void Remove(Blob toRemove)
+    {
+        blobList.Remove(toRemove);
+    }
+
+}
